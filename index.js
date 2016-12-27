1d1d5dfc18c8bc84b176254104ebcf73972b7e6e
Greg Wiley <aztec.rex@jammm.com>
2016-12-26 17:06:20 -0800

    flashes

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
-gpio = require("rpi-gpio");
+const gpio = require("rpi-gpio");
+const pin = 40;
 
-pin = 40;
-gpio.setup(pin, (err,res) => {
-  if (err) {
-    console.log("cannot setup gpio: ", err);
-    gpio.destroy();
-  } else {
-    gpio.write(pin, true, (err, res) => {
-      if (err) {
-        console.log("cannot write true to gpio: ", err);
-        gpio.destroy();
-      } else {
-        setTimeout(() => {
-          gpio.write(pin, false, (err, res) => {
-            if (err) {
-              console.log("cannot write false to gpio:", err);
-            }
-            gpio.destroy();
-          });
-        }
-        ,500);
-      }
-    });
+const write = (val, cb) => {
+  gpio.write(pin, val, err => {
+    if (err) {
+      console.log("cannot write ", val, " to ", pin);
+      cb(err, res);
+    } else cb(null, true)
   }
+};
+
+const init = cb => {
+  gpio.setup(pin, err => {
+    if (err) {
+      console.log("cannot setup ", pin);
+      cb(err);
+    } else cb(null, true);
+  });
+};
+
+const uninit = cb => {
+  gpio.destroy(err => {
+    if (err) cb(err);
+    else cb(null, true);
+  });
+};
+
+const delay = (duration, cb) => {
+  setTimeout(() => {
+    cb(null, true)
+  }, duration * 1000);
+};
+
+const flashes = (quantity, on_duration, off_duration, cb) => {
+
+    const perform = rem => {
+      if (rem > 0) {
+        write(true, err => {
+          if (err) cb(err);
+          else delay(on_duration, () => {
+            write(false, err => {
+              if (err) cb(err);
+              else delay(off_duration, ()) => {
+                perform(rem - 1);
+              });
+            });
+          });
+        });
+      } else {
+        cb(null, true);
+      }
+    }
+    perform(quantity);
+};
+
+init(err => {
+  err || flashes(20, .5, .5, ()) => {
+    uninit();
+  });
 });
